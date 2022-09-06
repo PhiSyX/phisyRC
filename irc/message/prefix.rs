9ef19ce3f58a77ae7ea3694703df200d57648197
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-09-07 01:49:53 +0200

    refactor(irc): préfixe d'un message -> builder state

--- irc/message/prefix.rs
+++ irc/message/prefix.rs
@@ -2,29 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+mod builder;
 mod host;
 mod nick;
+mod state;
 mod user;
 
 use core::fmt;
 use std::str::{Chars, FromStr};
 
-use lang::{codepoints::CodePoint, lexer::ParseState, stream::prelude::*};
+use lang::stream::prelude::*;
 
+use self::builder::ParsePrefixBuilder;
 pub(super) use self::{
 	host::IrcMessagePrefixHostError, nick::IrcMessagePrefixNickError,
 	user::IrcMessagePrefixUserError,
 };
-
-// --------- //
-// Structure //
-// --------- //
-
-#[derive(Default)]
-struct Builder {
-	temporary_buffer: String,
-	state: BuilderState,
-}
 
 // ----------- //
 // Énumération //
@@ -59,133 +52,17 @@ pub enum IrcMessagePrefixError {
 	InvalidOrigin(IrcMessagePrefixHostError),
 }
 
-#[derive(Debug)]
-#[derive(Default)]
-enum BuilderState {
-	#[default]
-	Initial,
-
-	User,
-	Server,
-}
-
 // -------------- //
 // Implémentation //
 // -------------- //
 
 impl IrcMessagePrefix {
 	pub fn parse(
-		input: &mut InputStream<Chars<'_>, char>,
+		stream: &mut InputStream<Chars<'_>, char>,
 	) -> Result<Self, IrcMessagePrefixError> {
-		let mut builder = Builder::default();
-
-		loop {
-			match builder.state {
-				| BuilderState::Initial => match input.consume_next()? {
-					// U+003A COLON (:)
-					//
-					// Ignorer le caractère.
-					| CodePoint::COLON => continue,
-
-					// Espaces blancs
-					//
-					// Arrêter l'analyse si le tampon temporaire vaut
-					// "localhost". Autrement, un préfixe NE PEUT PAS contenir
-					// d'espaces blancs. Il s'agira d'une erreur d'analyse.
-					| codepoint if codepoint.is_whitespace() => {
-						if builder.temporary_buffer == "localhost" {
-							break;
-						}
-						return Err(IrcMessagePrefixError::InvalidCharacter {
-							found: codepoint.unit(),
-							help: "Un point de code valide est attendu",
-						});
-					}
-
-					// U+0021 EXCLAMATION MARK (!)
-					// U+0040 COMMERCIAL AT (@)
-					//
-					// Passer à l'état [IrcMessagePrefixState::User].
-					| codepoint @ (CodePoint::EXCLAMATION_MARK
-					| CodePoint::COMMERCIAL_AT)
-						if !builder.is_empty() =>
-					{
-						builder.push(codepoint);
-						builder.state.switch(BuilderState::User);
-					}
-
-					// U+002E FULL STOP (.)
-					//
-					// Passer à l'état [IrcMessagePrefixState::Server].
-					| CodePoint::FULL_STOP => {
-						builder.push(CodePoint::FULL_STOP);
-						builder.state.switch(BuilderState::Server);
-					}
-
-					// Insérer tous les points de code valides dans le buffer
-					// temporaire.
-					| codepoint if codepoint.is_valid() => {
-						builder.push(codepoint);
-					}
-
-					| CodePoint::EOF if cfg!(test) => break,
-
-					// Tous autres caractères.
-					//
-					// Il s'agit d'une erreur d'analyse.
-					| _ => return Err(IrcMessagePrefixError::ParseError),
-				},
-
-				| BuilderState::User => match input.consume_next()? {
-					// Espaces blancs
-					//
-					// Un préfixe NE PEUT PAS contenir d'espaces blancs.
-					// Le préfixe est terminé, sortir de la boucle.
-					| codepoint if codepoint.is_whitespace() => break,
-
-					// U+0040 COMMERCIAL AT (@)
-					//
-					// Ajouter le caractère au buffer temporaire.
-					| CodePoint::COMMERCIAL_AT => {
-						builder.push(CodePoint::COMMERCIAL_AT);
-					}
-
-					// Tous les caractères valides.
-					//
-					// Ajouter le caractère au buffer temporaire.
-					| codepoint if codepoint.is_valid() => {
-						builder.push(codepoint);
-					}
-
-					// Tous autres caractères.
-					//
-					// Il s'agit d'une erreur d'analyse.
-					| _ => return Err(IrcMessagePrefixError::ParseError),
-				},
-
-				| BuilderState::Server => match input.consume_next()? {
-					// Espaces blancs
-					//
-					// Un préfixe NE PEUT PAS contenir d'espaces blancs.
-					// Le préfixe est terminé, sortir de la boucle.
-					| codepoint if codepoint.is_whitespace() => break,
-
-					// Tous les caractères valides.
-					//
-					// Ajouter le caractère au buffer temporaire.
-					| codepoint if codepoint.is_valid() => {
-						builder.push(codepoint);
-					}
-
-					// Tous autres caractères.
-					//
-					// Il s'agit d'une erreur d'analyse.
-					| _ => return Err(IrcMessagePrefixError::ParseError),
-				},
-			}
-		}
-
-		builder.build()
+		let mut builder = ParsePrefixBuilder::initialize(stream);
+		builder.analyze()?;
+		builder.finish()
 	}
 
 	pub fn parse_from_str(
@@ -250,90 +127,9 @@ impl IrcMessagePrefix {
 	}
 }
 
-impl Builder {
-	fn is_empty(&self) -> bool {
-		self.temporary_buffer.is_empty()
-	}
-
-	fn push(&mut self, codepoint: CodePoint<char>) {
-		self.temporary_buffer.push(codepoint.unit());
-	}
-
-	pub(super) fn build(
-		self,
-	) -> Result<IrcMessagePrefix, IrcMessagePrefixError> {
-		match self.state {
-			| BuilderState::Initial => {
-				// NOTE(phisyx): cas spécifique. Si on ne le fait pas, on tombe
-				// dans le cas de Self::User.
-				if self.temporary_buffer == "localhost" {
-					return Ok(IrcMessagePrefix::Server {
-						origin: self.temporary_buffer,
-					});
-				}
-
-				Ok(IrcMessagePrefix::User {
-					nick: nick::parse(&self.temporary_buffer)?,
-					user: Default::default(),
-					host: Default::default(),
-				})
-			}
-
-			| BuilderState::User => {
-				let mut prefix_user = IrcMessagePrefix::User {
-					nick: Default::default(),
-					user: Default::default(),
-					host: Default::default(),
-				};
-
-				if let Some((raw_nick_user, host)) = self
-					.temporary_buffer
-					.split_once('@')
-					.map(|(l, h)| (l, host::parse(h)))
-				{
-					if let Some((nick, user)) = raw_nick_user
-						.split_once('!')
-						.map(|(n, i)| (nick::parse(n), user::parse(i)))
-					{
-						prefix_user.set_nick(nick?);
-						prefix_user.set_user(user?);
-					} else {
-						prefix_user.set_nick(nick::parse(raw_nick_user)?);
-					}
-
-					prefix_user.set_host(host?);
-				} else if let Some((raw_nick, raw_user)) =
-					self.temporary_buffer.split_once('!')
-				{
-					prefix_user.set_nick(raw_nick);
-					prefix_user.set_user(raw_user);
-				}
-
-				prefix_user.check_fields()?;
-
-				Ok(prefix_user)
-			}
-
-			| BuilderState::Server => {
-				let prefix_server = IrcMessagePrefix::Server {
-					origin: self.temporary_buffer,
-				};
-				prefix_server.check_fields()?;
-				Ok(prefix_server)
-			}
-		}
-	}
-}
-
 // -------------- //
 // Implémentation // -> Interface
 // -------------- //
-
-impl ParseState for BuilderState {
-	fn switch(&mut self, new_state: Self) {
-		*self = new_state;
-	}
-}
 
 impl From<InputStreamError> for IrcMessagePrefixError {
 	fn from(_: InputStreamError) -> Self {

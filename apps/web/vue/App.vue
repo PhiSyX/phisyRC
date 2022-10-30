879b77207c644500c554cb3f606b113b6ad5a9fb
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-10-30 18:29:40 +0100

    refactor(apps): sépare les websocket de la vue

--- apps/web/vue/App.vue
+++ apps/web/vue/App.vue
@@ -1,34 +1,26 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue';
+import { State, useWebSocketStore } from './stores/websocket';
 
-let websocket = ref<WebSocket | null>(null);
-let websocket_uri = ref("ws://localhost:9667/");
+let websocket = useWebSocketStore(write_output);
+let websocket_url = ref("ws://localhost:9667/");
 
 let input = ref("hello world");
-let output = ref<any>([]);
 
-enum Output {
-	Connected = "Connecté",
-	Disconnected = "Déconnecté",
-	Error = "Erreur",
-	Send = "Envoyé",
-	Received = "Reçu",
+let output = ref<[State, ...Array<unknown>][]>([] as any);
+
+function handle_click_connect(_: MouseEvent) {
+	websocket.connect(websocket_url.value);
+
+	websocket.on("open", function () { // FIXME(phisyx): ne fonctionne pas en dehors du scope.
+		this.write("CAP LS 302");
+		this.write("NICK PhiSyX");
+		this.write("USER PhiSyX 8 * :utilisateur webSocket");
+	});
 }
 
-type FIXME = any;
-
-function handle_click_connect(evt: MouseEvent) {
-	websocket.value = new WebSocket(websocket_uri.value);
-	websocket.value.binaryType = "arraybuffer";
-
-	websocket.value.addEventListener("open", handle_open_connection);
-	websocket.value.addEventListener("close", handle_close_connection);
-	websocket.value.addEventListener("message", handle_message);
-	websocket.value.addEventListener("error", handle_error);
-}
-
-function handle_click_close(evt: MouseEvent) {
-	handle_close_connection(evt);
+function handle_click_close(_: MouseEvent) {
+	websocket.close();
 }
 
 function handle_submit(evt: Event) {
@@ -36,65 +28,17 @@ function handle_submit(evt: Event) {
 	write_socket_from_input();
 }
 
-function handle_open_connection(evt: Event) {
-	write_socket("USER PhiSyX 8 * :utilisateur webSocket");
-	write_socket("NICK PhiSyX");
-	write_output(Output.Connected);
-}
-
-function handle_close_connection(evt: CloseEvent | MouseEvent) {
-	write_output(Output.Disconnected);
-	if (websocket.value) {
-		websocket.value!.close();
-	}
-	websocket.value = null;
-}
-
-function handle_error(evt: Event) {
-	console.error(evt);
-	write_output(Output.Error);
-}
-
-function handle_message(evt: MessageEvent) {
-	let raw = evt.data;
-	if (raw instanceof Blob) {
-		let file_reader = new FileReader();
-		file_reader.addEventListener("loadend", handle_binary_input);
-		file_reader.readAsText(raw);
-	} else {
-		process(raw);
-	}
-}
-
-function handle_binary_input(evt: ProgressEvent<FileReader>) {
-	let file_reader = evt.target;
-	let raw = file_reader?.result! as ArrayBuffer;
-	process(raw);
-}
-
-function process(raw: NonNullable<ArrayBuffer>) {
-	if (!websocket.value) {
-		return;
-	}
-	let decoder = new TextDecoder();
-	let s = decoder.decode(raw);
-	write_output(Output.Received, s);
-}
-
-function write_socket(message: string) {
-	write_output(Output.Send, message);
-	let encoder = new TextEncoder();
-	let bytes = encoder.encode(`${message}\r\n`);
-	websocket.value!.send(bytes);
-}
-
 function write_socket_from_input() {
-	write_socket(`PRIVMSG #room :${input.value}`);
+	if (input.value.startsWith("/")) {
+		websocket.write(`${input.value.slice(1)}`);
+	} else {
+		websocket.write(`PRIVMSG #room :${input.value}`);
+	}
 	input.value = "";
 }
 
-function write_output(state: Output, ...args: FIXME) {
-	let item = [state, ...args];
+function write_output(state: State, ...args: Array<unknown>) {
+	let item: [State, ...Array<unknown>] = [state, ...args];
 	output.value.push(item);
 }
 </script>
@@ -103,8 +47,8 @@ function write_output(state: Output, ...args: FIXME) {
 	<h1>Test communication WebSocket</h1>
 
 	<form method="post" @submit="handle_submit">
-		<div class="form-group" v-if="!websocket">
-			<input v-model="websocket_uri" type="text">
+		<div class="form-group" v-if="!websocket.state.connection">
+			<input v-model="websocket_url" type="text">
 			<button type="button" @click.once="handle_click_connect">Se connecter</button>
 		</div>
 		<div class="form-group" v-else>
@@ -118,7 +62,7 @@ function write_output(state: Output, ...args: FIXME) {
 				</p>
 			</output>
 
-			<div class="form-group" v-show="websocket">
+			<div class="form-group" v-show="websocket.state.connection">
 				<input v-model="input" type="text">
 				<button type="submit">Envoie du message au serveur</button>
 			</div>
---
+++ apps/web/vue/stores/websocket.ts
@@ -0,0 +1,144 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { reactive, ref } from "vue";
+
+enum State {
+	Connected = "Connecté",
+	Disconnected = "Déconnecté",
+	Error = "Erreur",
+	Send = "Envoyé",
+	Received = "Reçu",
+}
+
+type WriteOutputFn = (_: State, ...args: unknown[]) => void;
+
+function useWebSocketStore(write_output_fn: WriteOutputFn) {
+	let ws = ref<WebSocket>();
+
+	// ------ //
+	// Export //
+	// ------ //
+
+	let state = reactive({ connection: 0 });
+
+	function connect(url: string | URL) {
+		ws.value = new WebSocket(url);
+		let lws = ws.value;
+		lws.binaryType = "arraybuffer";
+		lws.addEventListener("open", handle_open_connection(write_output_fn));
+		lws.addEventListener("close", handle_close_connection(write_output_fn));
+		lws.addEventListener("message", handle_message(write_output_fn));
+		lws.addEventListener("error", handle_error(write_output_fn));
+		state.connection = lws.readyState!;
+	}
+
+	function close() {
+		let lws = ws.value;
+		if (!lws) {
+			return;
+		}
+		lws.close();
+		ws.value = undefined;
+		state.connection = 0;
+	}
+
+	function write(data: string | ArrayBuffer) {
+		let lws = ws.value;
+		if (!lws) {
+			return;
+		}
+
+		write_output_fn(State.Send, data);
+
+		let encoder = new TextEncoder();
+		let bytes = encoder.encode(`${data}\r\n`);
+
+		lws.send(bytes);
+	}
+
+	function on<K extends keyof WebSocketEventMap>(
+		event: K,
+		listener: (
+			this: {
+				close: typeof close;
+				socket: WebSocket;
+				write: typeof write;
+			},
+			socket: {
+				close: typeof close;
+				socket: WebSocket;
+				write: typeof write;
+			},
+		) => any,
+	) {
+		let lws = ws.value;
+		if (!lws) {
+			return;
+		}
+		let cb = listener.bind({ close, socket: lws, write });
+		lws.addEventListener(event, function () {
+			cb({ close, socket: this, write });
+			state.connection = this.readyState!;
+		});
+	}
+
+	return { state, close, connect, on, write };
+}
+
+function handle_open_connection(write_output_fn: WriteOutputFn) {
+	return (evt: Event) => write_output_fn(State.Connected);
+}
+
+function handle_close_connection(write_output_fn: WriteOutputFn) {
+	return () => write_output_fn(State.Disconnected);
+}
+
+function handle_error(write_output_fn: WriteOutputFn) {
+	return (evt: Event) => {
+		console.error(evt);
+		write_output_fn(State.Error);
+	};
+}
+
+function handle_message(write_output_fn: WriteOutputFn) {
+	return (evt: MessageEvent) => {
+		let raw = evt.data;
+		if (raw instanceof Blob) {
+			let file_reader = new FileReader();
+			file_reader.addEventListener(
+				"loadend",
+				handle_binary_input(write_output_fn),
+			);
+			file_reader.readAsText(raw);
+		} else {
+			process(write_output_fn, raw);
+		}
+	};
+}
+
+function handle_binary_input(write_output_fn: WriteOutputFn) {
+	return (evt: ProgressEvent<FileReader>) => {
+		let file_reader = evt.target;
+		let raw = file_reader?.result! as ArrayBuffer;
+		process(write_output_fn, raw);
+	};
+}
+
+function process(
+	write_output_fn: WriteOutputFn,
+	raw: NonNullable<ArrayBuffer>,
+) {
+	let decoder = new TextDecoder();
+	let s = decoder.decode(raw);
+	write_output_fn(State.Received, s);
+}
+
+// ------ //
+// Export //
+// ------ //
+
+export { useWebSocketStore, State };

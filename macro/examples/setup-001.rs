91ededeaf5baee7222810312f71fd23e483c09d8
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-10-31 14:55:14 +0100

    example(macro): simple utilisation de la macro setup

---
+++ macro/examples/setup-001.rs
@@ -0,0 +1,9 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+#[phisyrc_macro::setup]
+fn main() {
+	println!("Hello World");
+}

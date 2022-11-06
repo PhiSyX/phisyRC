19c8c15ee02f43b0c0f63bfd054f6ead53bcd891
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-11-07 00:35:36 +0100

    feat(apps): affiche le logo pendant le chargement

--- apps/web/vue/pages/home.vue
+++ apps/web/vue/pages/home.vue
@@ -5,7 +5,12 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
 import Window from "~vue/Window.vue";
+
+onMounted(() => {
+	document.documentElement.dataset["js"] = "on";
+});
 </script>
 
 <template>

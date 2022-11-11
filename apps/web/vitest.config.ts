71fc880a28602e14b6ec29f883f3ca23c6a1218d
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-11-11 02:09:27 +0100

    chore: configure les tests

---
+++ apps/web/vitest.config.ts
@@ -0,0 +1,22 @@
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+import { defineConfig } from "vitest/config";
+
+import path from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	resolve: {
+		alias: [
+			{ find: /^~/, replacement: path.resolve("src") },
+			{ find: /^std/, replacement: path.resolve("std") },
+		],
+	},
+
+	test: {
+		includeSource: ["src/**/*.ts", "std/**/*.ts"],
+	},
+});

e2fd34a4f70749e1b372c0e99b31ce3128bef5c8
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-11-16 17:54:36 +0100

    refactor(npm): `capitalize` -- ajoute l'option `to_lower`
    Garde le reste de la chaîne de caractères intacte lorsque l'option `to_lower` vaut `false`.

--- npm/std/str/capitalize.ts
+++ npm/std/str/capitalize.ts
@@ -4,32 +4,56 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-/**
- * Remplace tous les premiers caractères de mots d'une phrase par une
- * majuscule, et le reste en minuscule.
- */
-function capitalize(s: str): string {
-	if (s.length === 0) {
-		return s;
-	}
+// ---- //
+// Type //
+// ---- //
 
-	const algo = (str: string) => {
+type Options = {
+	/// Remplace tout le reste d'une chaîne de caractères en minuscule.
+	to_lower?: bool;
+};
+
+// -------- //
+// Constant //
+// -------- //
+
+const SEPARATOR: RegExp = /([\s-_]+)/;
+
+const Default: Options = {
+	to_lower: true,
+};
+
+/**
+ * Remplace tous les premiers caractères des mots d'une chaîne de caractères |s|
+ * par une majuscule.
+ */
+function capitalize(s: str, options: Options = Default): str {
+	const algo = (s: str) => {
+		if (s.length === 0) {
+			return s;
+		}
+
 		// SAFETY: la condition ci-haut nous garantie que la chaîne de
-		// caractères comporte au moins 1 caractère, qui permet d'accéder à
-		// l'index 0 de la chaîne.
-		//
+		// caractères comporte au moins 1 caractère, qui nous permet d'accéder à
+		// l'index 0 de la chaîne en toute sécurité.
+		let first_ch = s[0].toUpperCase();
+
 		// NOTE(phisyx): le résultat d'une [String.prototype.slice(1)] lorsque
 		// la chaîne est vide, renvoie une chaîne vide.
-		return str[0].toUpperCase() + str.slice(1).toLowerCase();
-	};
+		let rest_of_str = s.slice(1);
 
-	const SEPARATOR = /([\s-_]+)/;
+		if (options.to_lower) {
+			return first_ch + rest_of_str.toLowerCase();
+		} else {
+			return first_ch + rest_of_str;
+		}
+	};
 
 	if (!SEPARATOR.test(s)) {
 		return algo(s);
 	}
 
-	return s.split(SEPARATOR).filter(Boolean).map(algo).join("");
+	return s.split(SEPARATOR).map(algo).join("");
 }
 
 // ------ //
@@ -45,11 +69,28 @@ export { capitalize };
 if (import.meta.vitest) {
 	const { it, expect } = import.meta.vitest;
 
-	it("capitalize", () => {
-		expect(capitalize("")).toBe("");
-		expect(capitalize("h")).toBe("H");
-		expect(capitalize("hello ")).toBe("Hello ");
-		expect(capitalize("hello world")).toBe("Hello World");
-		expect(capitalize("HELLO WORLD")).toBe("Hello World");
+	it("capitalize: vide", () => {
+		expect(capitalize("")).toEqual("");
+	});
+
+	it("capitalize: cas basiques", () => {
+		expect(capitalize(" ")).toEqual(" ");
+		expect(capitalize("h")).toEqual("H");
+		expect(capitalize("hello world")).toEqual("Hello World");
+		expect(capitalize("HELLO WORLD")).toEqual("Hello World");
+	});
+
+	it("capitalize: espace en trop", () => {
+		expect(capitalize("hello ")).toEqual("Hello ");
+
+		expect(capitalize("hello    ")).toEqual("Hello    ");
+		expect(capitalize("    hello    ")).toEqual("    Hello    ");
+	});
+
+	it("capitalize: garde le reste de la chaîne de caractères intacte", () => {
+		expect(capitalize(
+			"HeLLo WorLd",
+			{ to_lower: false }
+		),).toEqual("HeLLo WorLd");
 	});
 }

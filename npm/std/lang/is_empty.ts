8a8e966a005eb98a9c6f6eeb33da1eff27c3d5eb
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-11-28 20:17:35 +0100

    tests(npm): ajout des tests de la fonction `is_empty`

--- npm/std/lang/is_empty.ts
+++ npm/std/lang/is_empty.ts
@@ -4,6 +4,10 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+// ---- //
+// Type //
+// ---- //
+
 type OneOfProperty = {
 	count?: usize;
 	length?: usize
@@ -11,6 +15,47 @@ type OneOfProperty = {
 };
 
 /// Vérifie si la valeur passée par argument |value| est vide.
-export function is_empty<T extends OneOfProperty>(value: T): value is T {
+function is_empty<T extends OneOfProperty>(value: T): value is T {
 	return value.length === 0 || value.count === 0 || value.size === 0;
+}
+
+// ------ //
+// Export //
+// ------ //
+
+export { is_empty };
+
+// ---- //
+// Test //
+// ---- //
+
+if (import.meta.vitest) {
+	const { expect, it } = import.meta.vitest;
+
+	it("is_empty: .length", () => {
+		expect(is_empty("")).toBeTruthy();
+		expect(is_empty(" ")).toBeFalsy();
+	});
+
+	it("is_empty: .size", () => {
+		expect(is_empty(new Map())).toBeTruthy();
+
+		let map = new Map();
+		map.set(1, 1);
+		expect(is_empty(map)).toBeFalsy();
+	});
+
+	it("is_empty: .count", () => {
+		class CustomCount1 {
+			count: isize = 0;
+		}
+
+		expect(is_empty(new CustomCount1())).toBeTruthy();
+
+		class CustomCount2 {
+			count: isize = 1;
+		}
+
+		expect(is_empty(new CustomCount2())).toBeFalsy();
+	});
 }
